74242879dbcb05eb26df2ac9eed18cf17e7e469b
Nerajno <Nerajno@gmail.com>
2017-04-05 01:53:42 -0400

    still working on it

--- pieces.js
+++ pieces.js
@@ -15,23 +15,29 @@ function myFunction() {
   var ableToDrnk = year - urStuff;
   var other = year - ableToDrnk;
 
- //Esterlin- helped
+/*
+need to put in variables for each id
+var
 
-  if (other >= remainder){
-   document.getElementById('#two');
-  //console.log("hi");
- }else{
-  //console.log("bye");
-  document.getElementById('#three');
+*/
+
+
+ //Esterlin- helped
+ // got help again ..... 4417
+
+ if (other >= remainder){
+  document.getElementById('two');
+ //console.log("hi");
+}else{
+ //console.log("bye");
+ document.getElementById('three');
  };
+
+//This is for testing
  console.log(other);
-//getElementById within or instead of the console log.
 };
 // was trying to figure this out another way
-//$('.letSee').on("click",myFunction());
 //Making progress
 
 /*Notes/ Ideas
-
-/* onclick = take the input and minus it from today's date and if more than 21 give 1 message (yes,you can) else another message (you shall not pass) */
-  //console.log(year - urStuff);
+*onclick = take the input and minus it from today's date and if more than 21 give 1 message (yes,you can) else another message (you shall not pass) */
