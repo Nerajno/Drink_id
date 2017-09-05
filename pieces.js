6609bc6d5a8a9182f086829a575f87eb9ec936df
Nerajno <Nerajno@gmail.com>
2017-09-05 16:25:36 -0400

    tidied all

--- pieces.js
+++ pieces.js
@@ -1,49 +1,49 @@
 var myFunction = function() {
-    var today = new Date();
-    var year = today.getFullYear();
+  var today = new Date();
+  var year = today.getFullYear();
 
-    //This is the value/ age which will make someone able to drink... else they don't get to drink.
-    var remainder = 21;
+  //This is the value/ age which will make someone able to drink... else they don't get to drink.
+  var remainder = 21;
 
-    //This fetches the value of that was entered/input.
-    var urStuff = $("#birthYr").val();
+  //This fetches the value of that was entered/input.
+  var urStuff = $("#birthYr").val();
 
-    //This produces the birth year of the person
-    var ableToDrnk = year - urStuff;
-    var other = year - ableToDrnk;
+  //This produces the birth year of the person
+  var ableToDrnk = year - urStuff;
+  var other = year - ableToDrnk;
 
-    /*=> this if else statement takes their information and scroll to the other sections of the page based  on the id of the page sections*/
-    //Esterlin- helped and  got help again - Kenaan ..... 4417
+  /*=> this if else statement takes their information and scroll to the other sections of the page based  on the id of the page sections*/
+  //Esterlin- helped and  got help again - Kenaan ..... 4417
 
-    if (other >= remainder) {
+  if (other >= remainder) {
     $('body').animate({
-            scrollTop: $("#three").offset().top
-        }, 2000);
-        console.log("Leo");
-    } else {
-        $('body').animate({
-            scrollTop: $("#two").offset().top
-        }, 2000);
+      scrollTop: $("#three").offset().top
+    }, 2000);
+    console.log("Leo");
+  } else {
+    $('body').animate({
+      scrollTop: $("#two").offset().top
+    }, 2000);
 
-    };
+  };
 }
 //=> Running or rather calling the function.
 $("#move").click(myFunction);
 //This is for testing
 
-//=> this is the code for the back to top button, found on W3Schools site. 
+//=> this is the code for the back to top button, found on W3Schools site.
 //When the user scrools down from the top of the document, it shows the button.
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
 }
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-    document.documentElement.scrollTop = 0; // For IE and Firefox
+  document.body.scrollTop = 0; // For Chrome, Safari and Opera
+  document.documentElement.scrollTop = 0; // For IE and Firefox
 }
